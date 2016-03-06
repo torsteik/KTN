c7ae333ab347e16a7319af129b433bb6dd975d77
paalhm <paalma10sen@gmail.com>
2016-03-06 14:44:11 +0100

    Added files via upload

--- Parser.py
+++ Parser.py
@@ -1,35 +1,32 @@
 #Parser
 import json
 
-def parse_json(string):
+def parse_json(string, user):
     obj = json.loads(string)
     ret_val = ''
     if obj['response'] == 'error':
-        ret_val = '\n\n'
+        ret_val = '\n'
         ret_val += '*****\tERROR\t*****\n'
         ret_val += obj['sender'] + ': '
         ret_val += obj['content']
-        ret_val += '\n\n'
-        return ret_val
-    elif obj['response'] == 'message':
         ret_val += '\n'
+        return ret_val
+    elif (obj['response'] == 'message') and (user != obj['sender']):
         ret_val += obj['sender'] + ': '
         ret_val += obj['content']
-        ret_val += '\n'
         return ret_val
     elif obj['response'] == 'history':
-        ret_val = '\n\n'
+        ret_val = '\n'
         for jobj in obj['content']:
-            print jobj
-            ret_val += parse_json(json.dumps(jobj)) + '\n'
-        ret_val += '\n\n'
+            ret_val += parse_json(jobj)
+        ret_val += '\n'
         return ret_val
     elif obj['response'] == 'info':
-        ret_val = '\n\n'
+        ret_val = '\n'
         ret_val += '*****\tINFO\t*****\n'
         ret_val += obj['sender'] + ': '
         ret_val += obj['content']
-        ret_val += '\n\n'
+        ret_val += '\n'
         return ret_val
     else:
-        return '\n\nInvalid Response: "' + obj['response'] + '"\n\n'
+        return '\nInvalid Response: "' + obj['response'] + '"\n'
