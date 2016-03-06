4c2095465aea3551d5d0f9ece0fc309f97b9c43d
torsteik <tkvalvaag94@hotmail.com>
2016-03-06 20:38:50 +0100

    Update MessageReceiver.py

--- MessageReceiver.py
+++ MessageReceiver.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from threading import Thread
-import Parser
 import json
 import time
 
@@ -39,6 +38,40 @@ class MessageReceiver(Thread):
             else:
                 if json.loads(message)['response'] == 'history':
                     self.client.isLoggedin = True
-                print Parser.parse_json(message, self.client.username)
-
-
+                print self.parse_json(message, self.client.username)
+                
+    def parse_json(self, string, username):
+        obj = json.loads(string)
+        ret_val = ''
+        if obj['response'] == 'error':
+            ret_val = '\n'
+            ret_val += '*****\tERROR\t*****\n'
+            ret_val += obj['sender'] + ': '
+            ret_val += obj['content']
+            ret_val += '\n'
+            return ret_val
+        elif obj['response'] == 'message':
+            ret_val += '\n'
+            ret_val += obj['sender'] + ': '
+            ret_val += obj['content']
+            ret_val += '\n'
+            if obj['sender'] == username:
+                return ''
+            return ret_val
+        elif obj['response'] == 'history':
+            #print obj['content'][0]
+            ret_val = '\n*******************************'
+            ret_val += '\nPreviously on ' + obj['sender'] + "'s server:\n"
+            for jobj in obj['content']:
+                ret_val += self.parse_json(jobj, '')
+            ret_val += '*******************************\n'
+            return ret_val
+        elif obj['response'] == 'info':
+            ret_val = '\n'
+            ret_val += '*****\tINFO\t*****\n'
+            ret_val += obj['sender'] + ': '
+            ret_val += obj['content']
+            ret_val += '\n'
+            return ret_val
+        else:
+            return '\n\nInvalid Response: "' + obj['response'] + '"\n\n'
